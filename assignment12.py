8272d14bdf72821a59251ff1e6fb47cc0eb16b02
kkaustubhhchakraborty0508 <46235955+kkaustubhhchakraborty0508@users.noreply.github.com>
2018-12-29 15:53:47 +0530

    Q-12

---
+++ assignment12.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Q-12 write a program to accept a sentence from the user and reverse its each word By Kaustubh Chakraborty
+
+result=input("enter a string")
+words=result.split(" ")
+words.reverse()
+print(' '.join(words))
+
+
+# In[ ]:
+
+
+
+
