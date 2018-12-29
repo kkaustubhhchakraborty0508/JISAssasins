63053d8a764a20fc82a4b7a1356dd43f888161b5
kkaustubhhchakraborty0508 <46235955+kkaustubhhchakraborty0508@users.noreply.github.com>
2018-12-29 15:52:52 +0530

    Q-11

---
+++ assignment11.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Q-11 program to accept a string from user and accept a char value fro user and find out find out the total occurence
+#of char value in the string By Kaustubh Chakraborty
+str = input("Enter the string")
+char = input("Enter the character whose number of occurence is to be found in the string")
+c = str.count(char)
+print("Number of occurence of {} is {}".format(char ,c))
+
+
+# In[ ]:
+
+
+
+
