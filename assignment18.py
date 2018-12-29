2f7498270f2537fe226a2b43b0f7c15bb4482413
kkaustubhhchakraborty0508 <46235955+kkaustubhhchakraborty0508@users.noreply.github.com>
2018-12-29 16:16:26 +0530

    Q-18

---
+++ assignment18.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+# Q-18 write a program to calculate the x to the power y without  using the standard function
+
+
+
+num=int(input("enter a number"))
+num2=int(input("enter 2nd number"))
+a=1
+for num2 in range (1,num2+1):
+    a=(a*num)
+print("the value is :",a)
+
+
+# In[ ]:
+
+
+
+
