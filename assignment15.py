505f43fd7f545819b000b320e0c653256a92313e
kkaustubhhchakraborty0508 <46235955+kkaustubhhchakraborty0508@users.noreply.github.com>
2018-12-29 15:55:13 +0530

    Q-15

---
+++ assignment15.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[14]:
+
+
+# Q-15 write a program to calculate x^(y+z) By Kaustubh Chakraborty
+import math
+x=int(input("enter the value of x"))
+y=int(input("enter the value of y"))
+z=int(input("enter the input of z"))
+b=y+z
+a=pow(x,b)
+print("the value is ",a)
+
+
+# In[13]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
