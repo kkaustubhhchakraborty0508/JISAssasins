ed56b01a09253b59f5a6daef8253f8080f988158
kkaustubhhchakraborty0508 <46235955+kkaustubhhchakraborty0508@users.noreply.github.com>
2018-12-29 15:54:30 +0530

    Q-14

---
+++ assignment14.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+# Q-14 Program to accept 'n' numbers from user and store these numbers  into an array and count the number into an array and count the number of occurences of each number By Kaustubh Chakraborty
+from collections import Counter
+z=[]
+x=int(input("Enter the number of inputs u want to give"))
+for y in range(x):
+    a=int(input("Enter the number"))
+    z.append(a)
+Counter(z)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
