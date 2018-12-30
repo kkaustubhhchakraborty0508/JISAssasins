f4bbd61a93dc54cb16b8c5e9b800e46589eb077c
kkaustubhhchakraborty0508 <46235955+kkaustubhhchakraborty0508@users.noreply.github.com>
2018-12-30 10:01:25 +0530

    Add files via upload

---
+++ Assignment25.py
@@ -0,0 +1,9 @@
+#25 write a program to display whether the input character is a digit or alphabet.
+abc= (input("enter a character "))
+if ((abc>='a' and abc <='z')or (abc>='A' and abc<='Z')):
+    print(abc, "is an alphabet")
+else:
+    print(abc, "is a digit")
+
+
+
