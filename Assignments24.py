cbf776f5deeee6b4e5069b5410d5be1e6ef2816a
kkaustubhhchakraborty0508 <46235955+kkaustubhhchakraborty0508@users.noreply.github.com>
2018-12-30 10:06:23 +0530

    Q24

---
+++ Assignments24.py
@@ -0,0 +1,8 @@
+#24 Write a program to display the multiplication table of a given number.
+
+# To take input from the user
+num = int(input("Display multiplication table of? "))
+
+# use for loop to iterate 10 times
+for i in range(1, 11):
+   print(num,'x',i,'=',num*i)
