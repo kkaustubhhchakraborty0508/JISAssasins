f33c0b9c9f6873da8bb24580fe23f25e9e33ee27
kkaustubhhchakraborty0508 <46235955+kkaustubhhchakraborty0508@users.noreply.github.com>
2018-12-30 10:07:56 +0530

    Q20

---
+++ Assignment20.py
@@ -0,0 +1,4 @@
+#20 . program to accept a string from the user and replace all the occurences of character 'a' by '@' symbol.
+msg=" python is cooler than java."
+newmsg = msg.replace("a","@")
+print(newmsg)
